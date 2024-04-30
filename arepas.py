6f19dd817140e8d61f84f3d7dda82bc01e962f32
Fernando Torre Mora <ftorremora@usb.ve>
2024-04-30 07:22:45 -0400

    Add files via upload
    Agregando el programa de las arepas

---
+++ arepas.py
@@ -0,0 +1,13 @@
+"""Este es un programa para caluclar el volumen total
+de una arepa. Calcula la conversión de todas las unidades
+a tazas e imprime el resultado"""
+agua = 1
+harina = 1
+aceite = 1/16
+sal = 1/16 /3
+bol = harina+agua+sal
+budare = bol + aceite*0.75
+arepa = budare+1
+
+#este es el print
+print("arepa",arepa)
